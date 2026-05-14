aa19432fbd020d917616556ac46a1ec3e4fd52ac
Ashly <thegirlnextfloor@gmail.com>
2026-05-13 22:50:35 -0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "extension" ? "./" : "/StoryPlay/",
+  base: mode === "itch" ? "./" : "/StoryPlay/",
 }));
